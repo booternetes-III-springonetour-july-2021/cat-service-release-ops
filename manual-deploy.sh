295ae75b2185d0655ab1fe587312e35d7a9b8774
Cora Iberkleid <ciberkleid@gmail.com>
2021-07-03 01:06:49 -0400

    switch to non-release cat-service repo

--- manual-deploy.sh
+++ manual-deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Clone app
-rm -rf _temp-cat-service-release
-git clone https://github.com/booternetes-III-springonetour-july-2021/cat-service-release _temp-cat-service-release
-cd _temp-cat-service-release
+rm -rf _temp-cat-service
+git clone https://github.com/booternetes-III-springonetour-july-2021/cat-service _temp-cat-service
+cd _temp-cat-service
 
 # Build an publish image
 ./mvnw spring-boot:build-image \
